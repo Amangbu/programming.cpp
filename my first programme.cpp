81c2f5766c97c293c9d76f34bdfc0dcd3178acfe
AMAN <96134938+Amangbu@users.noreply.github.com>
2022-03-08 22:56:40 -0800

    Add files via upload
    here we all with me learn more and more.

---
+++ my first programme.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "20/UBS/010 AMAN \n";
+
+ cout << "I am student of B.Sc physical science \n";
+  cout << "Batch 2020-2023,IN GBU\n";
+   cout << "NOW START VARIABLES\n";
+  int A = 2582;
+int B = 2805;
+
+int sum = A + B; 
+cout << sum;
+
+ cout << "NOW USE MULTIPLE VARIABLES\n";
+ int P=9411,Q=0747,R=67,S=9111,T=0111,U=10;
+ cout << P+Q+R+S+T+U;
+ 
+ 
+
+  return 0;
+}
