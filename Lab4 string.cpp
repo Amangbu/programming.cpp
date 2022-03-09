cb68e9c3fdd0a9bed513b4b5700aabb10aa6b3c4
AMAN <96134938+Amangbu@users.noreply.github.com>
2022-03-08 23:08:42 -0800

    Add files via upload
    string is very very important part of programming language in C++.So learn and learn more successfully.

---
+++ Lab4 string.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+
+#include<string>
+using namespace std;
+
+int main() {
+	string university="GAUTAM BUDDHA UNIVERSITY\n";
+	cout<<university;
+	
+string school="SOVSAS\n";	
+  cout << school ;
+   string course="B.Sc physical science\n";
+  cout<<course;
+   string name="AMAN\n";
+   cout<<name;
+   string rollno="20/UBS/010\n";
+   cout<<rollno;
+   string subject1 = "PHYSICS ";
+string subject2 = "CHEMISTRY ";
+string subject3 = "MATHEMATICS ";
+string subject4 = "C++\n";
+string  subjects = subject1 + subject2 + subject3 + subject4;
+cout <<  subjects;
+string next="now read about ADDING NUMBERS AND STRING\n";
+   cout<<next;
+   int E = 1909;
+int F = 201;
+int G=109 ;
+int H =E + F + G ;
+cout<<H ;
+string now="NOW ADDING TWO STRING\n";
+cout<<now;
+string I = "945717";
+string J = "1748, NOW START STRING SIZE\n";
+string K = I + J;
+cout<<K;
+string sentence = "IN SESSION 2021-2022,B.Sc GROUP LEARNING C++ \n";
+cout << "The length of the sentence string is: " << sentence.size();
+string wow="NOW Start C++ Access String\n";
+cout<<wow;
+string myfullname = "CRASHANT\n";
+myfullname[0] = 'P';
+cout << myfullname;
+string yourfullname = "THE SCoENTIST\n";
+yourfullname[6] = 'I';
+cout<<yourfullname;
+
+
+
+
+ 
+   
+   
+
+  
+}
+
