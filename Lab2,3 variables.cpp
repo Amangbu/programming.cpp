b402a4b5e6527b3e0740365c31ea1f67e3a10fce
AMAN <96134938+Amangbu@users.noreply.github.com>
2022-03-08 23:06:41 -0800

    Add files via upload
    in 2nd of all, use variables in C++

---
+++ Lab2,3 variables.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+  cout << "20/UBS/010 AMAN \n";
+
+ 
+  double A = 25.82;
+double M = 28.05;
+
+double sum = A + M ;
+cout << sum; 
+string wishes = "CONGRATULATIONS FOR THIS GREAT EFFORTS"; 
+cout<<string(wishes);
+bool isMouseInputDevice = true;
+cout <<  isMouseInputDevice;
+
+
+
+}
+
+
